9d00c72656562912e7361c03d36f741ffaed77fb
bizarre <b@tumblbug.com>
2015-07-18 15:40:32 -0400

    /join to make new user

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   match 'login' => 'sessions#create', :via => :post
   match 'logout' => 'sessions#destroy', :via => :get
   match 'logout' => 'sessions#destroy', :via => :delete
+  match '/join' => 'users#new', :via => :get, :as => 'join'
   match '/:username' => 'users#show', :via => :get, :as => 'username'
 
 
